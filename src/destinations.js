1b62ee6aa0c36bf7a63edc9d55bfc84f261593fd
yogev83 <51350667+yogev83@users.noreply.github.com>
2022-09-13 10:32:27 -0700

    Add files via upload

--- src/destinations.js
+++ src/destinations.js
@@ -1,64 +1,64 @@
 export const destinations = {
-  RedwoodNtlPark: {
-    name: "Redwood National Park",
-    desc: "פארק לאומי עם העצים הכי גבוהים בעולם",
-    distance: 6.5,
-    website: "https://www.nps.gov/redw/index.htm",
-    attractions:
-      "הרבה טבע, הרבה שקט, עצים ענקיים,בעלי חיים,קו חוף יפה,נסיעה מאוד מיוחדת",
-    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
-    images: ["./redwoods1.jpeg", "./redwoods2.jpeg", "./redwoods3.jpeg"],
-    rating: 0,
-    visible: true,
-  },
-  MuirWoods: {
-    name: "Muir Woods",
-    desc: "יער של העצים מהגבוהים בעולם",
-    distance: 1,
-    website: "https://www.nps.gov/muwo/index.htm",
-    attractions:
-      "עצים גבוהים, מסלולי הליכה, נסיעה יפה בהרים מול הים, צילמו שם הרבה סרטים עם סצנות כביכול בחלל",
-    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
-    images: ["./muir1.jpeg", "./muir2.jpeg", "./muir3.jpeg"],
-    rating: 0,
-    visible: true,
-  },
-  SouthLakeTahoe: {
-    name: "South Lake Tahoe",
-    desc: "אחד האגמים היפים והמפורסמים בקליפורניה",
-    distance: 3.5,
-    website: "https://visitinglaketahoe.com/",
-    attractions:
-      "מים צלולים, מסלולי הליכה, נופים וטבע, סקי מים, רכבל שעולה להרים, רכבת הרים, אווירה כייפית, קייאקים",
-    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
-    images: ["./tahoe1.jpeg", "./tahoe2.jpeg", "./tahoe3.jpeg"],
-    rating: 0,
-    visible: true,
-  },
-  YosemiteNtlPark: {
-    name: "Yosemite National Park",
-    desc: "הפארק הלאומי המפורסם בעולם",
-    distance: 3,
-    website: "https://www.nps.gov/yose/index.htm",
-    attractions:
-      "עצי סקוייה ,צוקי גרניט ענקיים, נוף פראי מטורף, טבע ובעלי חיים, אגמים, מפלים, מסלולי הליכה וטיפוס",
-    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
-    images: ["./yosemite1.jpeg", "./yosemite2.jpeg", "./yosemite3.jpeg"],
-    rating: 0,
-    visible: true,
-  },
-  SequoiaNtlPark: {
-    name: "Sequoia National Park",
-    desc: "פארק עם העצים הגדולים בעולם",
-    distance: 4,
-    website: "https://www.nps.gov/seki/index.htm",
-    attractions:
-      "טבע ובעלי חיים, עצים ענקיים, נוף מהמם ,נסיעה עם הרכב דרך גזע עץ",
-    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
-    images: ["./sequioa1.jpeg", "./sequioa2.jpeg", "./sequioa3.jpeg"],
-    rating: 0,
-    visible: true,
-  },
+  // RedwoodNtlPark: {
+  //   name: "Redwood National Park",
+  //   desc: "פארק לאומי עם העצים הכי גבוהים בעולם",
+  //   distance: 6.5,
+  //   website: "https://www.nps.gov/redw/index.htm",
+  //   attractions:
+  //     "הרבה טבע, הרבה שקט, עצים ענקיים,בעלי חיים,קו חוף יפה,נסיעה מאוד מיוחדת",
+  //   map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+  //   images: ["./redwoods1.jpeg", "./redwoods2.jpeg", "./redwoods3.jpeg"],
+  //   rating: 0,
+  //   visible: true,
+  // },
+  // MuirWoods: {
+  //   name: "Muir Woods",
+  //   desc: "יער של העצים מהגבוהים בעולם",
+  //   distance: 1,
+  //   website: "https://www.nps.gov/muwo/index.htm",
+  //   attractions:
+  //     "עצים גבוהים, מסלולי הליכה, נסיעה יפה בהרים מול הים, צילמו שם הרבה סרטים עם סצנות כביכול בחלל",
+  //   map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+  //   images: ["./muir1.jpeg", "./muir2.jpeg", "./muir3.jpeg"],
+  //   rating: 0,
+  //   visible: true,
+  // },
+  // SouthLakeTahoe: {
+  //   name: "South Lake Tahoe",
+  //   desc: "אחד האגמים היפים והמפורסמים בקליפורניה",
+  //   distance: 3.5,
+  //   website: "https://visitinglaketahoe.com/",
+  //   attractions:
+  //     "מים צלולים, מסלולי הליכה, נופים וטבע, סקי מים, רכבל שעולה להרים, רכבת הרים, אווירה כייפית, קייאקים",
+  //   map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+  //   images: ["./tahoe1.jpeg", "./tahoe2.jpeg", "./tahoe3.jpeg"],
+  //   rating: 0,
+  //   visible: true,
+  // },
+  // YosemiteNtlPark: {
+  //   name: "Yosemite National Park",
+  //   desc: "הפארק הלאומי המפורסם בעולם",
+  //   distance: 3,
+  //   website: "https://www.nps.gov/yose/index.htm",
+  //   attractions:
+  //     "עצי סקוייה ,צוקי גרניט ענקיים, נוף פראי מטורף, טבע ובעלי חיים, אגמים, מפלים, מסלולי הליכה וטיפוס",
+  //   map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+  //   images: ["./yosemite1.jpeg", "./yosemite2.jpeg", "./yosemite3.jpeg"],
+  //   rating: 0,
+  //   visible: true,
+  // },
+  // SequoiaNtlPark: {
+  //   name: "Sequoia National Park",
+  //   desc: "פארק עם העצים הגדולים בעולם",
+  //   distance: 4,
+  //   website: "https://www.nps.gov/seki/index.htm",
+  //   attractions:
+  //     "טבע ובעלי חיים, עצים ענקיים, נוף מהמם ,נסיעה עם הרכב דרך גזע עץ",
+  //   map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+  //   images: ["./sequioa1.jpeg", "./sequioa2.jpeg", "./sequioa3.jpeg"],
+  //   rating: 0,
+  //   visible: true,
+  // },
 
   hearstCastle: {
     name: "Hearst Castle",
@@ -95,6 +95,100 @@ export const destinations = {
       "./carmel4.jpeg",
       "./carmel5.jpeg",
       "./carmel6.jpeg",
+    ],
+    rating: 0,
+    visible: true,
+  },
+
+  MonteryBay: {
+    name: "Monteray Bay",
+    desc: "עיירת חוף מגניבה",
+    distance: 2,
+    website: "https://www.seemonterey.com/",
+    attractions:
+      "מפרץ מונטריי, גלישת רוח, שיט צפייה בלוויתנים, לפעמים ניתן לראות לוויתנים מהחוף, אחד האקווריומים הגדולים והמפורסמים בארה״ב, קנארי-רו - הרחוב הראשי עם המון חנויות ומסעדות, פרפרי מונארך",
+    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+    images: [
+      "./monteray1.jpeg",
+      "./monteray2.jpeg",
+      "./monteray3.jpeg",
+      "./monteray4.jpeg",
+    ],
+    rating: 0,
+    visible: true,
+  },
+
+  BigSur: {
+    name: "Big Sur",
+    desc: "רצועת חוף מיוערת",
+    distance: 2.5,
+    website: "https://www.bigsurcalifornia.org/",
+    attractions:
+      "נקודות תצפית מדהימות על כביש 1, גשרים מטורפים, פוינט לובוס, אזור מיוער שקט עם עצים גדולים",
+    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+    images: ["./bigsur1.jpeg", "./bigsur2.jpeg", "./bigsur3.jpeg"],
+    rating: 0,
+    visible: true,
+  },
+
+  pasoRobels: {
+    name: "Paso Robels Night Lights",
+    desc: "שדה אורות מלאכותיים",
+    distance: 4,
+    website: "https://sensoriopaso.com/",
+    attractions: "שדה מדהים של אורות מלאכותיים, אוכל שתייה ומוזיקה חיה",
+    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+    images: ["./pr1.jpeg", "./pr2.jpeg", "./pr3.jpeg", "./pr4.jpeg"],
+    rating: 0,
+    visible: true,
+  },
+
+  pismoSanLuis: {
+    name: "Pismo Beach & San Luis Obispo",
+    desc: "עיר חוף יפהפיה",
+    distance: 4.5,
+    website: "https://www.slocity.org/",
+    attractions:
+      "חוף יפהיפה עם מפרץ גדול, קאייקים, מדרחוב מגניב עם מסעדות טובות, קיר ענק שמלא במסטיקים לעוסים",
+    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+    images: ["./slo1.jpeg", "./slo2.jpeg", "./slo3.jpeg"],
+    rating: 0,
+    visible: true,
+  },
+
+  Solvang: {
+    name: "Solvang",
+    desc: "עיירה דנית שהוקמה בידי יוצאי סקנדינביה",
+    distance: 5.5,
+    website: "https://www.solvangusa.com/",
+    attractions:
+      "מדרחוב מדהים ושונה מכל עיר אחרת בארה״ב, המון המון מאפיות סקנדינביות בכל מקום, תחנת רוח, ייקב",
+    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+    images: [
+      "./solvang1.jpeg",
+      "./solvang2.jpeg",
+      "./solvang3.jpeg",
+      "./solvang4.jpeg",
+      "./solvang5.jpeg",
+    ],
+    rating: 0,
+    visible: true,
+  },
+
+  SantaBarbra: {
+    name: "Santa Barbra",
+    desc: "עיר חוף מהמפורסמות בארה״ב",
+    distance: 6.5,
+    website: "https://santabarbaraca.com/",
+    attractions:
+      "חוף מהמם, שיט, צפייה בדולפינים, מסיון גדול ויפה, עיר ספרדית עתיקה, המון המון מסעדות ברמה גבוה, אווירה כייפית ויוקרתית",
+    map: "https://www.nationsonline.org/maps/Physical-World-Map-3360.jpg",
+    images: [
+      "./sb1.jpeg",
+      "./sb2.jpeg",
+      "./sb3.jpeg",
+      "./sb4.jpeg",
+      "./sb5.jpeg",
     ],
     rating: 0,
     visible: true,
